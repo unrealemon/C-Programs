0311a7c461d32ef14bfab733823dcf5d0b264b40
Md. Emon Ali <iamemon222@gmail.com>
2022-05-20 12:15:13 +0600

    Add files via upload

---
+++ uri1013.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, tmp;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a > b & a > c)
+        printf("%d eh o maior\n", a);
+    else  if(b > a & b > c)
+        printf("%d eh o maior\n", b);
+    else
+        printf("%d eh o maior\n", c);
+
+    return 0;
+}
